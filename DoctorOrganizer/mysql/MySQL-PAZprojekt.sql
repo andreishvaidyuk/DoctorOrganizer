9cd2d9a81b0a621c9122dd749e431a2c61811e0a
Patrik Rojek <Patrik Rojek@SAMSUNG-PC>
2016-12-05 17:29:09 +0100

    pridanie stlpcov patient na meno a priezvisko a stlpec time na cas terminu

--- DoctorOrganizer/mysql/MySQL-PAZprojekt.sql
+++ DoctorOrganizer/mysql/MySQL-PAZprojekt.sql
@@ -48,8 +48,10 @@ CREATE TABLE IF NOT EXISTS doctororganizer.term
 (
 id INT NOT NULL auto_increment,
 id_patient INT NOT NULL,
+patient VARCHAR(40) NOT NULL,
 id_doctor_office INT NOT NULL,
 date DATE NOT NULL,
+time VARCHAR(20) NOT NULL,
 reason VARCHAR(100),
 term_condition ENUM('schválený', 'neschválený', 'zrušený lekárom', 'zrušený pacientom'),
 PRIMARY KEY (id)
